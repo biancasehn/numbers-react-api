54dbed2a104d5c1aa86a20e21799e924148888ff
Bianca Sehn <sehnbiancarafaela135@gmail.com>
2021-04-13 17:46:46 +0200

    functions' optimizations

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
 import React from 'react';
 import { useState } from 'react';
-import { useForm } from "react-hook-form";
 
 import './App.css';
 
 function App() {
   const [fact, setFact] = useState("");
   const [inputSearch, setInputSearch] = useState("");
-  const [info, setInfo] = useState("year")
+  const [typeOfInfo, setInfo] = useState("year")
 
-  const { handleSubmit } = useForm();
-
-  
-  const infoSelector = (e) => {
+  const infoTypeSelector = (e) => {
     setInfo(e.target.defaultValue)
-    fetchingUrl(`https://numbersapi.com/${fact.number}/${e.target.defaultValue}?json`)
+    if (fact.number) {
+      fetchingUrl(`http://numbersapi.com/${fact.number}/${e.target.defaultValue}?json`)
+    }
   }
 
-  function onSubmit() {
-    fetchingUrl(`https://numbersapi.com/${inputSearch}/${info}?json`)
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchingUrl(`http://numbersapi.com/${inputSearch}/${typeOfInfo}?json`)
     setInputSearch("");
   }
 
-  const buttonClick = () => {
-    fetchingUrl(`https://numbersapi.com/random/${info}?json`)
+  const generateRandom = () => {
+    fetchingUrl(`http://numbersapi.com/random/${typeOfInfo}?json`)
   }
   
   function fetchingUrl(url) {
-    console.log(url);
     fetch(url)
       .then(res => res.json())
-      .then(result => setFact(result))
+      .then(quote => setFact(quote))
   }
 
   return (
     <div className="App">
-
       <h1 className="App-header"><strong>Choose the type of information and enter a number to learn a fact!</strong></h1>
-
-      <div className="checkBoxes" value={info} onChange={infoSelector}>
+      <div className="checkBoxes" value={typeOfInfo} onChange={infoTypeSelector}>
         <label>
           History
-          <input type="radio" value="year" defaultChecked={info==="year"} name="infotype" />
+          <input type="radio" value="year" defaultChecked={typeOfInfo==="year"} name="infotype" />
         </label>
         <label>
           Math
@@ -53,21 +49,22 @@ function App() {
         </label>
       </div>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit}>
         <input
-          type="text"
-          value={inputSearch}
-          placeholder='Enter a year'
+          type="number"
+          name='input-number'
+          placeholder='Enter a number/year'
           onChange={e => setInputSearch(e.target.value)}
+          value={inputSearch}
         />
-        <input type="submit" className="btn btnSubmit" value="Submit" />
+         <button type="submit" className="btn" >Submit</button>
       </form>
         
       <div className="factBox">
         <h3>{fact.text}</h3>
       </div>
 
-      <button className="btn btnRandom" onClick={buttonClick}>
+      <button className="btn btnRandom" onClick={generateRandom}>
         Random number
       </button>
 
